f7156b4020553ab8ca156c3e79996fb5b44bb864
Kareem Rashed <kareemrashed@aucegypt.edu>
2025-06-30 00:36:28 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cout << "No file path provided. Running default test program.\n";
 
-        file = "TestCases/ex-1.bin"; // Default test program file path
+        file = "TestCases/TC-ZX16-05.bin"; // Default test program file path
 
     } else {
         file = argv[1]; // Get the file path from the command-line argument
