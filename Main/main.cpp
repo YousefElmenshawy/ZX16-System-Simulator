f5eb997ce9e0965b9304d1eb89c0282c7e933077
Doha-Deia <doha_deia@aucegypt.edu>
2025-07-02 21:27:52 +0300

    Commenting assembly integration temporary

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,5 +1,46 @@
+// #include <iostream>
+// #include<cstdlib>
+// #include "ZX16_Simulator.h"
+// using namespace std;
+//
+// int main(int argc, char* argv[]) {
+//     std::string file;
+//     if (argc < 2) {
+//         std::cout << "No file path provided. Running default test program.\n";
+//         std::string asmFile = "../TestCases/TC-ZX16-03.s";
+//         std::string binFile = "../TestCases/TC-ZX16-03.bin";
+//         // Assemble the default test case using the assembler script
+//         // This assumes you are running the executable from: Main/cmake-build-debug/
+//         std::string command = "python ../../assembler/zx16asm.py " + asmFile + " -o " + binFile;
+//         int status = system(command.c_str());
+//         if (status != 0) {
+//             std::cerr << "Default assembler failed.\n";
+//             return 1;
+//         }
+//         file=binFile;
+//     }
+//     else {
+//         std::string asmFile = argv[1];  // e.g. "prog.asm"
+//         std::string binFile = asmFile.substr(0, asmFile.find_last_of(".")) + ".bin";
+//
+//         std::string command = "python ../../assembler/zx16asm.py " + asmFile + " -o " + binFile;
+//         int status = system(command.c_str());
+//         if (status != 0) {
+//             std::cerr << "Assembler failed.\n";
+//             return 1;
+//         }
+//
+//         file = binFile;
+//     }
+//
+//     ZX16_Simulator sim;
+//     sim.loadBinaryFile(file);    // Load the binary program file
+//     sim.run();// run the simulator
+//
+//     return 0;
+// }
 #include <iostream>
-#include<cstdlib>
+
 #include "ZX16_Simulator.h"
 using namespace std;
 
@@ -7,32 +48,13 @@ int main(int argc, char* argv[]) {
     std::string file;
     if (argc < 2) {
         std::cout << "No file path provided. Running default test program.\n";
-        std::string asmFile = "../TestCases/TC-ZX16-03.s";
-        std::string binFile = "../TestCases/TC-ZX16-03.bin";
-        // Assemble the default test case using the assembler script
-        // This assumes you are running the executable from: Main/cmake-build-debug/
-        std::string command = "python ../../assembler/zx16asm.py " + asmFile + " -o " + binFile;
-        int status = system(command.c_str());
-        if (status != 0) {
-            std::cerr << "Default assembler failed.\n";
-            return 1;
-        }
-        file=binFile;
+
+        //file = "TestCases/TC-ZX16-05.bin"; // Default test program file path
+        file = "../TestCases/TC-ZX16-03.bin";
+
+    } else {
+        file = argv[1]; // Get the file path from the command-line argument
     }
-    else {
-        std::string asmFile = argv[1];  // e.g. "prog.asm"
-        std::string binFile = asmFile.substr(0, asmFile.find_last_of(".")) + ".bin";
-
-        std::string command = "python ../../assembler/zx16asm.py " + asmFile + " -o " + binFile;
-        int status = system(command.c_str());
-        if (status != 0) {
-            std::cerr << "Assembler failed.\n";
-            return 1;
-        }
-
-        file = binFile;
-    }
-
     ZX16_Simulator sim;
     sim.loadBinaryFile(file);    // Load the binary program file
     sim.run();// run the simulator
